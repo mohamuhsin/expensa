98dc1536c198ab4a51f5cf84c1d4815ad2fcd431
mohamuhsin <muhsinohoho@outlook.com>
2023-05-10 12:13:41 +0300

    first component

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 const ExpenseItem = () => {
-  return <div>Expense Item</div>;
+  return (
+    <div>
+      <div>March 29th 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <h2>$294.4</h2>
+      </div>
+    </div>
+  );
 };
-
 
 export default ExpenseItem;

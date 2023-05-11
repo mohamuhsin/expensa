c5dfc3b142986e2cd17dd63d4dac71bbebb19175
mohamuhsin <muhsinohoho@outlook.com>
2023-05-11 11:58:14 +0300

    Created expenses component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
-
+import Expenses from "./components/Expenses/Expenses";
 function App() {
   const expense = [
-    { title: "Car Insurance", amount: 294.00, date: new Date(2022, 9, 28) },
+    { title: "Car Insurance", amount: 294.0, date: new Date(2022, 9, 28) },
     { title: "Toilet Paper", amount: 24.16, date: new Date(2021, 4, 28) },
-    { title: "New Tv", amount: 94.70, date: new Date(2022, 10, 28) },
+    { title: "New Tv", amount: 94.7, date: new Date(2022, 10, 28) },
     { title: "New Desk", amount: 29.12, date: new Date(2021, 5, 28) },
   ];
 
   return (
     <div>
       <h2>Expensa App</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
+      <Expenses items={expense} />
     </div>
   );
 }

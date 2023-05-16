28af8811ff35e1201524d6b6bb747f06f39f8a3a
mohamuhsin <muhsinohoho@outlook.com>
2023-05-16 14:50:54 +0300

    corrected some errors

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
 
 const NewExpense = (props) => {
-const [isEditing, setIsEditing] = useState('false');
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-   props.onAddExpense(expenseData);
+    props.onAddExpense(expenseData);
   };
 
   const startEditingHandler = () => {
     setIsEditing(true);
-  }
+  };
   return (
     <div className="new-expense">
-     {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
       {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />}
     </div>
   );

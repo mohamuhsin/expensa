3cf1dd406cab9b43c900655d4854ff610fb6b716
mohamuhsin <muhsinohoho@outlook.com>
2023-05-20 20:32:32 +0300

    gave props to the chartbar component

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,12 +1,20 @@
 import React from "react";
-import './Chart.css';
+import "./Chart.css";
 import ChartBar from "./ChartBar";
 
 const Chart = (props) => {
-  return <div className="chart">
-{props.dataPoints.map(dataPoint => <ChartBar value={dataPoint.value} maxValue={null} label={dataPoint.value} />)}
-
-  </div>;
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.value}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Chart;

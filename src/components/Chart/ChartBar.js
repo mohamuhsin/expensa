a7a20d5c4aef038bcd59ff937bd84d8a39735db7
mohamuhsin <muhsinohoho@outlook.com>
2023-05-20 20:42:26 +0300

    created custom chartBar compoennt

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,12 +1,19 @@
 import React from "react";
-import './ChartBar.css';
+import "./ChartBar.css";
 
 const ChartBar = (props) => {
-  return <div className="chart-bar">
-    <div className="chart-bar_-inner">
-        <div className="chart-bar__fill"></div>
-        <div className="chart-bar__label"></div>
+  let barFillHeight = "0%";
+  if (props.max > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar_-inner">
+        <div className="chart-bar__fill" style={{}}></div>
+        <div className="chart-bar__label">{props.label}</div>
+      </div>
     </div>
-  </div>;
+  );
 };
 export default ChartBar;

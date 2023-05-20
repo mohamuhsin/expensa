af40533da1dcf78d3212cf2cbcf9c213a8a196e0
mohamuhsin <muhsinohoho@outlook.com>
2023-05-20 20:17:30 +0300

    created charts bar

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Chart = () => {
-    return(<div></div>);
-}
+  return <div></div>;
+};
 
 export default Chart;
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
 const ChartBar = () => {
-    return(<div></div>)
-}
-export default ChartBar
+  return <div></div>;
+};
+export default ChartBar;

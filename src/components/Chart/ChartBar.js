442efa776a6e487670c298cdb70ca4b7bd04e7fe
mohamuhsin <muhsinohoho@outlook.com>
2023-05-20 20:35:52 +0300

    created custom chartBar compoennt

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,6 +1,12 @@
 import React from "react";
+import './ChartBar.css';
 
-const ChartBar = () => {
-  return <div></div>;
+const ChartBar = (props) => {
+  return <div className="chart-bar">
+    <div className="chart-bar_-inner">
+        <div className="chart-bar__fill"></div>
+        <div className="chart-bar__label"></div>
+    </div>
+  </div>;
 };
 export default ChartBar;
